e5f319145a0e196a91fbbcdc2e28a312e95f9111
Peter Zaharia <peton3@gmail.com>
2016-02-12 17:35:41 +0200

    Updated the album initialization

--- src/App/GalleryBundle/Resources/public/js/app.js
+++ src/App/GalleryBundle/Resources/public/js/app.js
@@ -1,59 +1,6 @@
 $(function(){
-    var Image = Backbone.Model.extend({
-        defaults: {
-            src: '/upload/gallery/empty.jpg',
-            text: '',
-            order: 0
-        }
-    });
+    window.pageImages = new ImageCollection();
+    var galleryApp = new AppView({images: window.pageImages});
 
-    var ImageView = Backbone.View.extend({
-        tagName: 'div',
-        className: 'album-image',
-        events: {
-            'click' : function(){
-                alert(this.model.text)
-            }
-        },
-        initialize: function(){
-            this.listenTo(this.model, 'change', this.render);
-        },
-        render: function(){
-            this.$el.html('<li><div class="image-block"><img src="'+this.model.get('src')+'"><\/div><p>'+this.model.get('text')+'</p></li>');
-            return this;
-        }
-    });
-
-    var Album = Backbone.Collection.extend({
-        model: Image,
-        url: '/gallery/rpc/',
-        /*initialize: function(){
-            this.on('all', function(a1, a2, a3){
-                console.log(a1, a2, a3);
-            });
-        }*/
-    });
-
-    var PageAlbum = new Album;
-
-    var AppView = Backbone.View.extend({
-        el: $('#album-wrapper > ul'),
-
-        initialize: function(){
-            this.listenTo(PageAlbum, 'add', this.addOne);
-
-            PageAlbum.fetch();
-        },
-
-        addOne: function(image){
-            var newImage = new ImageView({model: image}).render().el;
-            this.$el.append(newImage);
-        }
-
-        /*render: function(){
-
-         }*/
-    });
-
-    var App = new AppView;
-})
+    //setTimeout("window.pageImages.reset([{'text' : 'test'}]);", 3000);
+});
--- src/App/GalleryBundle/Resources/public/js/views/appView.js
+++ src/App/GalleryBundle/Resources/public/js/views/appView.js
@@ -1,18 +1,36 @@
 var AppView = Backbone.View.extend({
     el: $('#album-wrapper > ul'),
 
-    initialize: function(){
-        this.listenTo(PageAlbum, 'add', this.addOne);
+    images: {},
 
-        PageAlbum.fetch();
+    initialize: function(options){
+        if (options.images) {
+            this.setImages(options.images);
+        }
+    },
+
+    setImages: function(images){
+        this.images = images;
+        this.listenTo(this.images, 'add', this.addOne);
+        this.listenTo(this.images, 'reset', this.onReset);
+        //this.listenTo(this.images, 'all', this.log);
+        this.images.fetch();
+    },
+    log: function(a1, a2, a3){
+      console.log(a1, a2, a3);
     },
 
     addOne: function(image){
         var newImage = new ImageView({model: image}).render().el;
         this.$el.append(newImage);
+    },
+
+    addBatch: function(){
+        this.images.each(this.addOne, this);
+    },
+
+    onReset: function(){
+        this.$el.find('li').remove();
+        this.addBatch();
     }
-
-    /*render: function(){
-
-     }*/
 });
